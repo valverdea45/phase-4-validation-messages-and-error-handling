05e1d593860f034e598aaf5d2b40368c49801522
Adrian <adrianvalverde45@gmail.com>
2023-07-28 11:51:33 -0400

    commit tree d1eeb45a356b21cf32e6a264b45372fb0cc60e8e

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   wrap_parameters false
+
   
   def index
     movies = Movie.all
@@ -7,8 +8,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
